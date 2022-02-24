dc6f44d648a803fdc6f552915cd4e3cf5815a097
殊怀丶 <lvzhihe_123@qq.com>
2022-02-24 12:52:55 +0800

    added risk

--- pages/realTimeMap/realTimeMap.js
+++ pages/realTimeMap/realTimeMap.js
@@ -11,6 +11,20 @@ Page({
     type: null,
     hastap: false,
     picurl: "",
+    tags:[
+      {
+        url:"../../images/marker_high.png",
+        meaning:"高风险"
+      },
+      {
+        url:"../../images/marker_mid.png",
+        meaning:"中风险"
+      },
+      {
+        url:"../../images/marker_sus.png",
+        meaning:"疑似"
+      }
+    ]
   },
   onLoad: function (e) {
     this.setData({
@@ -32,7 +46,7 @@ Page({
     wx.getLocation({
       type: 'gcj02',
       success(res) {
-        console.log(res)
+        //console.log(res)
         that.setData({
           latitude: res.latitude,
           longitude: res.longitude
@@ -54,17 +68,18 @@ Page({
   },
   getAllTracksInCoordinates: function () {
     var that = this;
+    var templist = [];
+    var tmp2 = [];
+    var tmpcircles = [];
     wx.request({
-      url: getApp().globalData.url + "Track/getAllTracksInCoordinates",
+      url: getApp().globalData.url + "RealTime/getAllSuspectedLocationsInCoordinates",
       header: {
         'content-type': 'application/json' // 默认值
       },
       success(res) {
-        var templist = [];
-        var tmp2 = [];
-        var tmpcircles = [];
-        console.log(res);
-        for (var i = 0; i < res.data.data.length; i++) {
+        console.log(res)
+        var i;        
+        for (i = 0; i < res.data.data.length; i++) {
           if (!res.data.data[i].mapResponseDto.status) {
             templist.push({
               id: i,
@@ -73,7 +88,7 @@ Page({
               width: 50,
               height: 50,
               joinCluster: true,
-              iconPath: getApp().globalData.picurl + "images/marker.png",
+              iconPath: "../../images/marker_sus.png",
               callout: {
                 content: res.data.data[i].locationName, //每个位置的地点名称
                 display: "BYCLICK",
@@ -87,31 +102,138 @@ Page({
                 borderRadius: 10
               }
             });
-            var typetmp = res.data.data[i].mapResponseDto.result.level;
+            //var typetmp = res.data.data[i].mapResponseDto.result.level;
             tmp2.push({
               id: i,
-              typ: typetmp == "NoClass" ? "暂无" : typetmp //场所类型，对应level
+              typ: "疑似" 
             });
             tmpcircles.push({
               latitude: res.data.data[i].mapResponseDto.result.location.lat,
               longitude: res.data.data[i].mapResponseDto.result.location.lng,
-              fillColor: '#E77C8E32',    //#E77C8E32  rgba(231,124,142,0.2)
-              color: '#E77C8E',      //#E77C8E  rgba(231,124,142,1)
+              fillColor: '#70809032',    //#E77C8E32  rgba(231,124,142,0.2)
+              color: '#708090',      //#E77C8E  rgba(231,124,142,1)
               radius: 1000,
               strokeWidth: 2
-            });
+            });            
           }
         }
-        that.setData({
-          markers: templist,
-          id_type: tmp2,
-          circles: tmpcircles
-        })
-        console.log(templist)
-        //onsole.log(tmp2)
-        //console.log(tmpcircles)
+        //第二部分由此开始,中风险
+        wx.request({
+          url: getApp().globalData.url + "RealTime/getMediumRiskPlace",
+          data:{
+            pageNum:1,
+            pageSize:60
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res2) {
+            console.log(res2)
+            var j;        
+            for (j = 0; j < res2.data.data.length;j++) {
+              if (!res2.data.data[j].mapResponseDto.status) {
+                templist.push({
+                  id: j+i,
+                  latitude: res2.data.data[j].mapResponseDto.result.location.lat,
+                  longitude: res2.data.data[j].mapResponseDto.result.location.lng,
+                  width: 50,
+                  height: 50,
+                  joinCluster: true,
+                  iconPath: "../../images/marker_mid.png",
+                  callout: {
+                    content: res2.data.data[j].riskPlace.areaName, //每个位置的地点名称
+                    display: "BYCLICK",
+                    textAlign: "center",
+                    color: "#000000",
+                    borderWidth: 1,
+                    borderColor: "#808080",
+                    bgColor: "#e9e7ef",
+                    padding: 3,
+                    fontSize: 15,
+                    borderRadius: 10
+                  }
+                });
+                //var typetmp = res.data.data[i].mapResponseDto.result.level;
+                tmp2.push({
+                  id:j+i,
+                  typ: "中风险" 
+                });
+                tmpcircles.push({
+                  latitude: res2.data.data[j].mapResponseDto.result.location.lat,
+                  longitude: res2.data.data[j].mapResponseDto.result.location.lng,
+                  fillColor: '#FFD70032',    //#E77C8E32  rgba(231,124,142,0.2)
+                  color: '#FFD700',      //#E77C8E  rgba(231,124,142,1)
+                  radius: 1000,
+                  strokeWidth: 2
+                });            
+              }
+            }
+            //第三部分由此开始，高风险
+            wx.request({
+              url: getApp().globalData.url + "RealTime/getHighRiskPlace",
+              data:{
+                pageNum:1,
+                pageSize:20
+              },
+              header: {
+                'content-type': 'application/json' // 默认值
+              },
+              success(res3) {
+                console.log(res3)
+                var k;        
+                for (k = 0; k < res3.data.data.length;k++) {
+                  if (!res3.data.data[k].mapResponseDto.status) {
+                    templist.push({
+                      id: j+i+k,
+                      latitude: res3.data.data[k].mapResponseDto.result.location.lat,
+                      longitude: res3.data.data[k].mapResponseDto.result.location.lng,
+                      width: 50,
+                      height: 50,
+                      joinCluster: true,
+                      iconPath: "../../images/marker_high.png",
+                      callout: {
+                        content: res3.data.data[k].riskPlace.areaName, //每个位置的地点名称
+                        display: "BYCLICK",
+                        textAlign: "center",
+                        color: "#000000",
+                        borderWidth: 1,
+                        borderColor: "#808080",
+                        bgColor: "#e9e7ef",
+                        padding: 3,
+                        fontSize: 15,
+                        borderRadius: 10
+                      }
+                    });
+                    //var typetmp = res.data.data[i].mapResponseDto.result.level;
+                    tmp2.push({
+                      id:j+i+k,
+                      typ: "高风险" 
+                    });
+                    tmpcircles.push({
+                      latitude: res3.data.data[k].mapResponseDto.result.location.lat,
+                      longitude: res3.data.data[k].mapResponseDto.result.location.lng,
+                      fillColor: '#E77C8E32',    //#E77C8E32  rgba(231,124,142,0.2)
+                      color: '#E77C8E',      //#E77C8E  rgba(231,124,142,1)
+                      radius: 1000,
+                      strokeWidth: 2
+                    });            
+                  }
+                } 
+                // console.log(templist)
+                // console.log(tmp2)
+                // console.log(tmpcircles)
+                that.setData({
+                  markers: templist,
+                  id_type: tmp2,
+                  circles: tmpcircles
+                })
+              }
+            });
+          }
+        });
       }
-    })
+    });    
+    
   },
   //进行经纬度转换为距离的计算
   Rad: function (d) {
@@ -131,7 +253,7 @@ Page({
     return s;
   },
   clickmarker: function (e) {
-    console.log(e)
+    //console.log(e)
     for (var p = 0; p < this.data.markers.length; p++) {
       if (e.detail.markerId == this.data.markers[p].id) {
         var la = this.data.markers[p].latitude;
